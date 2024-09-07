e4548ae2341f95196eb1a1228200225b06f5ff70
datsrt02 <63861231+datsrt02@users.noreply.github.com>
2024-09-07 15:03:41 +0700

    Update node-red.sh

--- node-red.sh
+++ node-red.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
-# Copyright (c) 2021-2024 tteck
-# Author: tteck (tteckster)
-# License: MIT
-# https://github.com/tteck/Proxmox/raw/main/LICENSE
 
 function header_info {
 clear
@@ -60,7 +55,7 @@ UPD=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "SUPPORT" --radio
   3>&1 1>&2 2>&3)
 header_info
 if [ "$UPD" == "1" ]; then
-  if [[ "$(node -v | cut -d 'v' -f 2)" == "18."* ]]; then
+  if [[ "$(node -v | cut -d 'v' -f 2)" == "14."* ]]; then
     if ! command -v npm >/dev/null 2>&1; then
       msg_info "Installing NPM"
       apt-get install -y npm >/dev/null 2>&1
